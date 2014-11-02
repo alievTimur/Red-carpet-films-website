927587a188b6fee4a54af2a5a4cd55d1985db50d
alievTimur <ta1g13@ecs.soton.ac.uk>
2014-11-02 16:05:14 +0000

    included js files

--- includes/header.php
+++ includes/header.php
@@ -10,7 +10,9 @@
   <title>| Timur Aliev's website</title>
   <!-- Bootstrap -->
   <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
-  <link href="css/main.css" rel="stylesheet" type="text/css"><!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <link href="css/main.css" rel="stylesheet" type="text/css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
   <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
